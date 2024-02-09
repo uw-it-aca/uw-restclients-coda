e07ee68186909d6cd7aae33f511403cb620998d4
Fang Lin <fanglin@uw.edu>
2024-02-09 11:38:42 -0800

    upgrade to Python 3.10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='UW-RestClients-CoDa',
     version=VERSION,
     packages=['uw_coda'],
-    author="UW-IT AXDD",
+    author="UW-IT T&LS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=['UW-RestClients-Core'],
@@ -35,7 +35,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
     ],
 )
 
